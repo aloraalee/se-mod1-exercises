081e5074db6cd297db1e3dfee7cdae7064d651db
Alora Riley <alora.riley@gmail.com>
2024-08-30 08:48:21 -0600

    Finish class

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,23 @@
 class Venue
+    attr_reader :name, :capacity, :patrons
+    
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(patron)
+        @patrons << patron
+    end
+
+    def yell_at_patrons
+        @patrons.map(&:upcase)
+    end
+
+
+
+
+
+
 end
