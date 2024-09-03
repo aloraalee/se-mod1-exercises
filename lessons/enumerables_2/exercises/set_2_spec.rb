25c1049dc028bff9c5cfee39d9f387b7d3333d09
Alora Riley <alora.riley@gmail.com>
2024-09-03 14:16:30 -0600

    Pass tests and add notes

--- lessons/enumerables_2/exercises/set_2_spec.rb
+++ lessons/enumerables_2/exercises/set_2_spec.rb
@@ -5,7 +5,7 @@ describe "#sort/#sort_by" do
   it "sorts the numbers" do 
     numbers = [8, 23, 7, 29, 9, 10]
 
-    sorted_numbers = #your code here 
+    sorted_numbers = numbers.sort
 
     expect(sorted_numbers).to eq([7, 8, 9, 10, 23, 29])
   end
@@ -13,7 +13,7 @@ describe "#sort/#sort_by" do
   it "sorts the names alphabetically" do 
     names = ["Leonardo", "Donatello", "Michaelangelo", "Raphael"]
 
-    sorted_names = #code
+    sorted_names = names.sort
 
     expect(sorted_names).to eq(["Donatello", "Leonardo", "Michaelangelo", "Raphael"])
   end
@@ -21,7 +21,9 @@ describe "#sort/#sort_by" do
   it "sorts the names by name length" do 
     names = ["Leonardo", "Donatello", "Michaelangelo", "Raphael"]
 
-    sorted_names = #code
+    sorted_names = names.sort_by do |name|
+      name.length
+    end
 
     expect(sorted_names).to eq(["Raphael", "Leonardo", "Donatello", "Michaelangelo", ])
   end
@@ -35,8 +37,26 @@ describe "#sort/#sort_by" do
         ["Stella", 8]
       ]
   
-    sorted_names = #code here
+      people_in_order = people.sort_by do |person|
+       person[1]
+        # require 'pry' ; binding.pry
+        # In pry, the first time tyoe person to see what happens. We then learn we want the second element in the array so we use [1]
+        # In pry again, we type people_in_order to see what this looks like. we get ["Sofie", 4], ["Stella", 8], ["Scarlett", 9]. 
+        #This is close, but it still includes the corresponding value. 
+      end
 
+    sorted_names = people_in_order.map do |person|
+     # require 'pry' ; binding.pry
+     person[0]
+    end
+
+      # We want to return names in an array, in order of the second value of the subarrays. 
+      # Let's solve this in two steps. 
+      # 1. Lets sort the srray by values
+      # 2. Iterate again, using map just return the names only for each element
+
+
+      
     expect(sorted_names).to eq(["Sofie", "Stella", "Scarlett"])
   end
 end
