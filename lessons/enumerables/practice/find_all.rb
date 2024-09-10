964be79c59fcd0c3f0a24ce82a028ed4c9ccb268
Alora Riley <alora.riley@gmail.com>
2024-09-09 22:47:55 -0600

    Finish find_all

--- lessons/enumerables/practice/find_all.rb
+++ lessons/enumerables/practice/find_all.rb
@@ -13,7 +13,7 @@ def find_all_even_numbers # done for you
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     numbers.find_all do |number|
-      # Your code goes here
+      number.odd?
     end
 
     #  expected return value is [1, 3, 5, 7, 9]
@@ -22,7 +22,9 @@ def find_all_even_numbers # done for you
   def find_all_words_with_three_letters
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.length == 3
+    end
 
     #  expected return value is ["bad", "cat", "dog", "red"]
   end
@@ -30,7 +32,9 @@ def find_all_even_numbers # done for you
   def find_all_words_with_more_than_three_letters
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.length > 3
+    end
 
     #  expected return value is ["pill", "finger", "blue", "table"]
   end
@@ -38,7 +42,9 @@ def find_all_even_numbers # done for you
   def find_all_words_ending_in_e
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.end_with? "e"
+    end
 
     #  expected return value is ["are", "strike", "piece", "warble", "pipe"]
   end
@@ -46,7 +52,9 @@ def find_all_even_numbers # done for you
   def find_all_words_ending_in_ing
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.end_with? "ing"
+    end
 
     #  expected return value is ["bring", "singing"]
   end
@@ -54,7 +62,9 @@ def find_all_even_numbers # done for you
   def find_all_words_containing_e
     words = ["four", "red", "five", "blue", "pizza", "purple"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.include? "e"
+    end
 
     #  expected return value is ["red", "five", "blue", "purple"]
   end
@@ -62,7 +72,10 @@ def find_all_even_numbers # done for you
   def find_all_dinosaurs
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
 
-    # Your code goes here
+    animals.find_all do |animal|
+      dinosaur = animal.end_with? "saurus"
+      dinosaur
+    end
 
     #  expected return value is ["tyrannosaurus", "achillesaurus", "qingxiusaurus"]
   end
@@ -70,7 +83,9 @@ def find_all_even_numbers # done for you
   def find_all_floats
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
 
-    # Your code goes here
+    numbers.find_all do |number|
+      number.kind_of? Float
+    end
 
     #  expected return value is [1.4, 3.5, 4.9, 9.1, 8.0]
   end
@@ -78,7 +93,9 @@ def find_all_even_numbers # done for you
   def find_all_arrays
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
 
-    # Your code goes here
+    elements.find_all do |element|
+      element.kind_of? Array 
+    end
 
     #  expected return value is [["dog"], [56, 3, 8]]
   end
@@ -86,21 +103,23 @@ def find_all_even_numbers # done for you
   def find_all_hashes
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
 
-    # Your code goes here
+    elements.find_all do |element|
+      element.kind_of? Hash 
+    end
 
     #  expected return value is [{:dog=>"fido"}, {:stuff=>"things"}]
   end
 
 
 
-# p find_all_even_numbers
-# p find_all_odd_numbers
-# p find_all_words_with_three_letters
-# p find_all_words_with_more_than_three_letters
-# p find_all_words_ending_in_e
-# p find_all_words_ending_in_ing
-# p find_all_words_containing_e
-# p find_all_dinosaurs
-# p find_all_floats
-# p find_all_arrays
-# p find_all_hashes
+p find_all_even_numbers
+p find_all_odd_numbers
+p find_all_words_with_three_letters
+p find_all_words_with_more_than_three_letters
+p find_all_words_ending_in_e
+p find_all_words_ending_in_ing
+p find_all_words_containing_e
+p find_all_dinosaurs
+p find_all_floats
+p find_all_arrays
+p find_all_hashes
