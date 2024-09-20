ae84b559104f043c28cfe3dd9a689219a996dcb2
Alora Riley <alora.riley@gmail.com>
2024-09-20 15:33:58 -0600

    Pass all unskipped test

--- ruby_exercises/mythical-creatures/spec/wizard_spec.rb
+++ ruby_exercises/mythical-creatures/spec/wizard_spec.rb
@@ -22,31 +22,35 @@ RSpec.describe Wizard do
     expect(wizard.bearded?).to be false
   end
 
-  it 'has root powers' do
+  xit 'has root powers' do
     wizard = Wizard.new('Stella', bearded: false)
     expect(wizard.incantation('chown ~/bin')).to eq('sudo chown ~/bin')
   end
 
-  it 'has many root powers' do
+  xit 'has many root powers' do
     wizard = Wizard.new('Sal', bearded: true)
     expect(wizard.incantation('rm -rf /home/mirandax')).to eq('sudo rm -rf /home/mirandax')
   end
 
   it 'starts rested' do
-    # create wizard
-    # .rested? returns true
+    wizard = Wizard.new('Sal', bearded: true)
+    expect(wizard.rested?).to eq(true)
   end
 
   it 'can cast spells' do
-    # create wizard
-    # .cast returns "MAGIC MISSILE!"
+    wizard = Wizard.new('Sal', bearded: true)
+    expect(wizard.cast).to eq("MAGIC MISSILE!")
   end
 
   it 'gets tired after casting three spells' do
-    # create wizard
-    # casts spell twice
-    # check if wizard is rested
-    # casts spell
-    # check wizard is not rested
+    wizard = Wizard.new('Sal', bearded: true)
+    wizard.cast
+    wizard.cast
+
+    expect(wizard.rested?).to eq(true)
+
+    wizard.cast
+
+    expect(wizard.rested?).to eq(false)
   end
 end
