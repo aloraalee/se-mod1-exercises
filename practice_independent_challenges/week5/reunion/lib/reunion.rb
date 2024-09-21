10f44980381a2e29cb6f13acf2db0fbba4388655
Alora Riley <alora.riley@gmail.com>
2024-09-21 13:34:55 -0600

    Add reunion_total_cost method and helper method reunion_cost _hash

--- practice_independent_challenges/week5/reunion/lib/reunion.rb
+++ practice_independent_challenges/week5/reunion/lib/reunion.rb
@@ -12,4 +12,20 @@ class Reunion
     @activities << activity
   end
 
+
+  def reunion_cost_hash
+    reunion_cost_hash = {}
+
+    @activities.each do |activity|
+      reunion_cost_hash[activity.name] = activity.total_cost
+    end
+
+    reunion_cost_hash
+  end
+
+  def reunion_total_cost
+    reunion_cost_hash.values.sum
+  end
+
+  
 end
--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Reunion do
   before(:each) do
     @reunion = Reunion.new("1406 BE")
     @activity_1 = Activity.new("Brunch")
+    @activity_2 = Activity.new("Drinks")
   end
 
   describe '#initialize' do
@@ -21,6 +22,23 @@ RSpec.describe Reunion do
     @reunion.add_activity(@activity_1)
 
     expect(@reunion.activities).to eq([@activity_1])
+  end
+
+  it 'can return the total cost of the reunion' do
+    @reunion.add_activity(@activity_1)
+    @reunion.add_activity(@activity_2)
+
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+
+    expect(@activity_1.total_cost).to eq(60)
+
+    @activity_2.add_participant("Maria", 20)
+    @activity_2.add_participant("Luther", 10)
+
+    expect(@activity_2.total_cost).to eq(30)
+
+    expect(@reunion.reunion_total_cost). to eq(90)
   end
 
 end
