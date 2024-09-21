72fe86e6c8bfc3f35328ddbd95b7aca501da61ed
Alora Riley <alora.riley@gmail.com>
2024-09-21 14:29:56 -0600

    Add capture_stdout to help with expeccted output

--- practice_independent_challenges/week5/reunion/spec/spec_helper.rb
+++ practice_independent_challenges/week5/reunion/spec/spec_helper.rb
@@ -2,6 +2,14 @@ require 'pry'
 require './lib/activity'
 require './lib/reunion'
 
+def capture_stdout
+  original_stdout = $stdout
+  $stdout = StringIO.new
+  yield
+  $stdout.string
+ensure
+  $stdout = original_stdout
+end
 
 RSpec.configure do |config|
   config.formatter = :documentation
