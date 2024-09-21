c318591709bfb09eb52fb0336e7f252f1618608c
Alora Riley <alora.riley@gmail.com>
2024-09-21 12:50:39 -0600

    Initialize

---
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe Reunion do
+  before(:each) do
+    @reunion = Reunion.new("1406 BE")
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@reunion).to be_an_instance_of(Reunion)
+      expect(@reunion.name).to eq("1406 BE")
+    end
+  end
