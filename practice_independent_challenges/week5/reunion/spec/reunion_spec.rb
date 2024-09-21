46ac592e95509b8a154ed21f5a0461ece6174505
Alora Riley <alora.riley@gmail.com>
2024-09-21 14:34:08 -0600

    Test for the print summary

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -38,7 +38,45 @@ RSpec.describe Reunion do
 
     expect(@activity_2.total_cost).to eq(30)
 
+    expect(@reunion.reunion_cost_hash).to eq({"Brunch"=>60, "Drinks"=>30})
+
     expect(@reunion.reunion_total_cost). to eq(90)
   end
+
+  it 'can return a hash with the participants name as a key and 
+    what they owe for the whole reunion as a value' do
+      @reunion.add_activity(@activity_1)
+      @reunion.add_activity(@activity_2)
+  
+      @activity_1.add_participant("Maria", 20)
+      @activity_1.add_participant("Luther", 40)
+      @activity_2.add_participant("Maria", 20)
+      @activity_2.add_participant("Luther", 10)
+  
+      expect(@reunion.total_owed_per_participant).to eq({"Maria" => 5, "Luther" => -5})
+    end
+
+    it 'can print a summary of each participants name and what they owe,
+      separated by a line break' do
+
+        @reunion.add_activity(@activity_1)
+        @reunion.add_activity(@activity_2)
+    
+        @activity_1.add_participant("Maria", 20)
+        @activity_1.add_participant("Luther", 40)
+        @activity_2.add_participant("Maria", 20)
+        @activity_2.add_participant("Luther", 10)
+
+        expected_output = [
+          "Maria: owes $5.00",
+          "Luther: is owed $5.00"
+        ]
+
+        output = capture_stdout { @reunion.print_summary }
+
+        actual_lines = output.split("\n").map(&:strip)
+
+        expect(actual_lines).to eq(expected_output)
+      end
 
 end
